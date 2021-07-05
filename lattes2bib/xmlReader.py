88ab6c04beca08c43943644146b924f540745fa0
Ellen Junker <ellenjunker2000@gmail.com>
2021-07-05 16:03:07 -0300

    Organize xmlReader file

--- lattes2bib/xmlReader.py
+++ lattes2bib/xmlReader.py
@@ -8,21 +8,21 @@ class XmlFile():
 		self.myroot = self.mytree.getroot()
 
 		# Each type of work has its entry type and its fields (tags)
-		# The first item on the list is the entry type and the second item is a class that defines the fields (as well as its path in the xml file)
-		self.entryTypes = {'TRABALHOS-EM-EVENTOS':['inproceedings', Event()], 
-						   'ARTIGOS-PUBLICADOS':['article', Article()], 
-						   'LIVROS-PUBLICADOS-OU-ORGANIZADOS':Book(), 
-						   'CAPITULOS-DE-LIVROS-PUBLICADOS':Chapter(), 
-		 				   'TEXTOS-EM-JORNAIS-OU-REVISTAS':['article', Text()], 
-		 				   'DEMAIS-TIPOS-DE-PRODUCAO-BIBLIOGRAFICA':['article', Other()]} 
+		# xml tag: [bib signature, class with the tags]
+		self.entryTypes = {'TRABALHOS-EM-EVENTOS': ['inproceedings', Event()], 
+						   'ARTIGOS-PUBLICADOS': ['article', Article()], 
+						   'LIVROS-PUBLICADOS-OU-ORGANIZADOS': Book(), 
+						   'CAPITULOS-DE-LIVROS-PUBLICADOS': Chapter(), 
+		 				   'TEXTOS-EM-JORNAIS-OU-REVISTAS': ['article', Text()], 
+		 				   'DEMAIS-TIPOS-DE-PRODUCAO-BIBLIOGRAFICA': ['article', Other()]} 
 
 		self.outputDir = outputDir
 
 	def getWorks(self): # Generates the bib files
 		for category in self.myroot[1]: # -> Produção Bibliográfica
 			if category.tag == 'LIVROS-E-CAPITULOS':
-				for i in category:
-					if i.tag == 'LIVROS-PUBLICADOS-OU-ORGANIZADOS': # Separates books from chapters
+				for i in category: # Separates books from chapters
+					if i.tag == 'LIVROS-PUBLICADOS-OU-ORGANIZADOS': 
 						books = i
 						bib = BibFile('book', books, self.entryTypes[i.tag], self.outputDir)
 					else:
@@ -31,7 +31,7 @@ class XmlFile():
 			else:
 				if category.tag in self.entryTypes.keys():
 					pub = category # List of publications of a category
-					pubType = self.entryTypes[category.tag] # Get the data of the category (its entry type and its fields(tags))
+					pubType = self.entryTypes[category.tag] # Get the data of the category (its bib signature and its fields (tags))
 					bib = BibFile(pubType[0], pub, pubType[1], self.outputDir) # Entry type, publications, fields, output directory
 				else:
 					print('Categoria não inclusa na implementação: ', category.tag)
