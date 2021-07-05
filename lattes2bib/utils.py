eae88098333438940d62ab79682119cc22efa724
Ellen Junker <ellenjunker2000@gmail.com>
2021-07-05 15:54:12 -0300

    Organize utils file

--- lattes2bib/utils.py
+++ lattes2bib/utils.py
@@ -1,30 +1,21 @@
 import re
 
 def checkException(word): # Checa se a palavra corresponde a uma exceção (artigo, preposição ou conjunção)
-	exceptionsPort = ['o', 'os', 'a', 'as', 'um', 'uns', 'uma', 'umas',
-			          'ao', 'aos', 'à', 'às', 'do', 'dos', 'da', 'das',
-			          'dum', 'duns', 'duma', 'dumas', 'no', 'nos', 'na',
-			          'nas', 'num', 'nuns', 'numa', 'numas', 'pelo', 'pelos', 
-			          'pela', 'pelas', 'ante', 'após', 'até', 'com', 'de', 
-			          'em', 'para', 'por', 'sem', 'sob', 'trás', 'e', 'nem', 
-			          'mas', 'ou', 'já', 'que', 'pois']
+	exceptionsPort = ['o', 'os', 'a', 'as', 'um', 'uns', 'uma', 'umas', 'ao', 'aos', 'à', 'às', 'do', 'dos', 'da', 'das', 'dum', 'duns', 'duma', 'dumas', 'no', 'nos', 'na', 'nas', 'num', 'nuns', 'numa', 'numas', 'pelo', 'pelos', 'pela', 'pelas', 'ante', 'após', 'até', 'com', 'de', 'em', 'para', 'por', 'sem', 'sob', 'trás', 'e', 'nem', 'mas', 'ou', 'já', 'que', 'pois']
 	
 	exceptionsEng = ['and', 'but', 'or', 'so', 'the', 'a', 'an', 'of', 'in', 
 	                 'on', 'to', 'for', 'at', 'as', 'with']
 
-	exceptionsEsp = ['a', 'de', 'en', 'por', 'con', 'para', 'sin', 'y', 'ni',
-					 'que', 'así', 'no', 'o', 'ya', 'el', 'él', 'la', 'lo', 
-					 'los', 'las', 'un', 'una', 'unos', 'unas']
+	exceptionsEsp = ['a', 'de', 'en', 'por', 'con', 'para', 'sin', 'y', 'ni', 'que', 'así', 'no', 'o', 'ya', 'el', 'él', 'la', 'lo', 'los', 'las', 'un', 'una', 'unos', 'unas']
 
-
-	if word.lower() in exceptionsEng or word.lower() in exceptionsEsp or word.lower() in exceptionsPort:
+	if word.lower() in zip(exceptionsEng, exceptionsEsp, exceptionsPort):
 		return True
 	else:
 		return False
 
 
 def checkForAbbrev(word): # Checa se a palavra corresponde a alguma sigla conhecida
-	abbrevs = ['IEEE', 'ACM','MDPI', 'VLSI', 'IFIP', 'UFSM', 'UFSC', 'UFRJ', 'USP', 'UNIVALI']
+	abbrevs = ['IEEE', 'ACM', 'MDPI', 'VLSI', 'IFIP', 'UFSM', 'UFSC', 'UFRJ', 'USP', 'UNIVALI']
 	if word.upper() in abbrevs:
 		word = word.upper()
 
@@ -73,7 +64,7 @@ def titleCase(text):
 
 	lowercase_words = re.split(" ", text.lower())
 	final_words = [lowercase_words[0].capitalize()]
-	final_words += [word if checkException(word) == True else word.capitalize() for word in lowercase_words[1:]]
+	final_words += [word if checkException(word) is True else word.capitalize() for word in lowercase_words[1:]]
 	final_words = treatAbbrev(final_words)
 	final_text = " ".join(final_words)
 
