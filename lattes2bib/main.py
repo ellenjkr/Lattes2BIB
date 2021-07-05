c326dbaecbc739c69680fbb5196f579786d46123
Ellen Junker <ellenjunker2000@gmail.com>
2021-07-05 15:41:21 -0300

    Organize main

--- lattes2bib/main.py
+++ lattes2bib/main.py
@@ -1,28 +1,31 @@
-from xmlReader import XmlFile
 import glob
-import sys
-from errno import EEXIST
 import os
-from os import makedirs,path
 import re 
 
+from errno import EEXIST
+from os import makedirs
+from os import path
+from xmlReader import XmlFile
+
 def mkdir_p(pathName): # Creates a directory (mkdir -p on cmd)
-    try:
-        makedirs(pathName)
-    except OSError as exc:
-        if exc.errno == EEXIST and path.isdir(pathName):
-            pass
-        else:
-        	raise
+	try:
+		makedirs(pathName)
+	except OSError as exc:
+		if exc.errno == EEXIST and path.isdir(pathName):
+			pass
+		else:
+			raise exc
+
 
 if __name__ == '__main__':
-	filepath = 'Resumes' 
+	filepath = 'Resumes'
+
 	for fileAddress in glob.glob(os.path.join(filepath, '*.xml')): # For each resume on the path
-		inputDir = fileAddress # inputDir = Resumes/filename.xml
+		inputDir = fileAddress # input directory = Resumes/filename.xml
 
 		fileName = re.findall(r'\\(.*).xml', fileAddress) # Resumes/fileName.xml -> fileName
-		outputDir = f'Results/{fileName[0]}' # .bib you'll be saved on a folder that has the name of the resyne, on the folder "Results"
-		mkdir_p(outputDir) # Creates the folder that has the name of the resume
+		outputDir = f'Results/{fileName[0]}' # .bib files will be saved to a folder that has the name of the resume, on the folder "Results"
+		mkdir_p(outputDir) # Creates the folder that will hold the .bib files
 
 		works = XmlFile(inputDir, outputDir) # Reads xml file
 		works.getWorks() # Get the works from the xml file
